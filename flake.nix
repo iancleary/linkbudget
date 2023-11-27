a5d812a3ca7b6389d0e3b57e614c59682dc36ed1
Ian Cleary <github@iancleary.me>
2023-11-27 12:13:35 -0700

    Update rust flake (#24)

--- flake.nix
+++ flake.nix
@@ -9,6 +9,8 @@
     flake-utils.url = "github:numtide/flake-utils";
     # A utility library for working with Rust
     rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
@@ -25,13 +27,22 @@
         # Use the specific version of the Rust toolchain specified by the toolchain file
         localRust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-        # Other utilities commonly used in Rust projects (but not in this example project)
-        others = with pkgs; [ just ];
+        # Cargo subcommands commonly used in Rust projects
+        cargoSubCommands = with pkgs; [
+          cargo-audit
+          cargo-edit
+          cargo-release
+          cargo-watch
+        ];
+
+        # Placeholder for utilities commonly used in Rust projects (but not in this example project)
+        others = with pkgs; [ ];
+
       in {
         devShells = {
           default = pkgs.mkShell {
             # Packages included in the environment
-            buildInputs = [ localRust ] ++ others;
+            buildInputs = [ localRust ] ++ cargoSubCommands ++ others;
 
             # Run when the shell is started up
             shellHook = ''
